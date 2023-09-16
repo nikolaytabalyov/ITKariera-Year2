08359effe6abe61d5b7a89d888d622b6e94831dc
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-09-16 12:00:05 +0300

    added exercise 3 to DefiningClasses 02.2

---
+++ Modul 5/DefiningClasses/02.2/3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _3 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<RationalNumber> rationalNums = new();
+
+            int[] arguments = Console.ReadLine().Split(' ').Where(x => x != "").Select(int.Parse).ToArray();
+            for (int i = 0; i < arguments.Length; i += 2) { 
+                rationalNums.Add(new RationalNumber(arguments[i], arguments[i + 1]));
+            }
+
+            foreach (var num in rationalNums) {
+                int nod = num.BiggestDivider();
+                num.Numerator /= nod;
+                num.Denumerator /= nod;
+                num.Print();
+                if (rationalNums.IndexOf(num) != rationalNums.Count - 1) {
+                    Console.Write("; ");
+                }
+            }
+        }
+    }
+}
---
+++ Modul 5/DefiningClasses/02.2/3/RationalNumber.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3 {
+    class RationalNumber {
+        private int numerator;
+        private int denumerator;
+        public RationalNumber(int numerator, int denumerator) {
+            Numerator = numerator;
+            Denumerator = denumerator;
+        }
+
+        public int Numerator { get; set; }
+        public int Denumerator {
+            get => denumerator;
+            set {
+                if (value == 0)
+                    throw new ArgumentException("Denumerator cannot be 0!");
+                else
+                    denumerator = value;
+            }
+        }
+
+        public void Print() {
+            Console.Write($"{Numerator}/{Denumerator}");
+        }
+
+        public int BiggestDivider() {
+            int nod = 1;
+            int r = 0;
+            int a = Numerator;
+            int b = Denumerator;
+
+            while (b != 0) {
+                r = a % b;
+                a = b;
+                b = r;
+            }
+
+            nod = a;
+            return nod;
+        }
+    }
+}
