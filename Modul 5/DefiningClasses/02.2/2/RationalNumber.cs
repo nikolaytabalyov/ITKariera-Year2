4130ad96d1ce3d9295706dcee2223cdc7ea3a3b0
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-09-16 11:34:40 +0300

    added exercise 2 to DefiningClasses 02.2

---
+++ Modul 5/DefiningClasses/02.2/2/RationalNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2 {
+    class RationalNumber {
+        private int numerator;
+        private int denumerator;
+        public RationalNumber(int numerator, int denumerator) {
+            Numerator = numerator;
+            Denumerator = denumerator;
+        }
+
+        public int Numerator { get; set; }
+        public int Denumerator {
+            get => denumerator;
+            set {
+                if (value == 0)
+                    throw new ArgumentException("Denumerator cannot be 0!");
+                else
+                    denumerator = value;
+            }
+        }
+
+        public void Print() {
+            Console.Write($"{Numerator}/{Denumerator}");
+        }
+    }
+}
