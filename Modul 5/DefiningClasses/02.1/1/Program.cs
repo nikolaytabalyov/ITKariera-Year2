43d15109ed78798ae30047fc01dd580caca83129
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-09-16 10:14:04 +0300

    addded 1st exercise to Defining Classes 02.1

---
+++ Modul 5/DefiningClasses/02.1/1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace _1 {
+    class Program {
+        static void Main(string[] args) {
+            Type personType = typeof(Person);
+            FieldInfo[] fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            Console.WriteLine(fields.Length);
+
+            Person person1 = new() { age = 12, name = "Pesho" };
+            Person person2 = new() { age = 18, name = "Gosho" };
+            Person person3 = new() { age = 25, name = "Stamat" };
+            Console.WriteLine($"Name: {person1.name}, Age: {person1.age}");
+            Console.WriteLine($"Name: {person2.name}, Age: {person2.age}");
+            Console.WriteLine($"Name: {person3.name}, Age: {person3.age}");
+
+
+        }
+    }
+}
