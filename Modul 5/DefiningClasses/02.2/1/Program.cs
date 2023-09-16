4b05d0e38a09fdc55fced65da7a0340d316dbcf4
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-09-16 11:10:55 +0300

    added exercise 1 to DefiningClasses 02.2

---
+++ Modul 5/DefiningClasses/02.2/1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _1 {
+    internal class Program {
+        static void Main(string[] args) {
+            int n = 3;
+            List<RationalNumber> rationalNums = new();
+            for (int i = 0; i < n; i++) {
+                int numerator = Convert.ToInt32(Console.ReadLine());
+                int denumerator = Convert.ToInt32(Console.ReadLine());
+
+                rationalNums.Add(new RationalNumber(numerator, denumerator));
+            }
+
+            foreach (var num in rationalNums) {
+                num.Print();
+            }
+        }
+    }
+}
