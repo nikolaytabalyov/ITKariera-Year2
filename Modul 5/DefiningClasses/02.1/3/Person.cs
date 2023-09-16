efca923e61b9ba3a648fc70085c9240a5d609b4e
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-09-16 10:48:44 +0300

    addded exercise 3 to DefiningClasses 02.1

---
+++ Modul 5/DefiningClasses/02.1/3/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace _3 {
+    class Person {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age) {
+            Name = name;
+            Age = age;
+        }
+
+        public void PrintAbove30() {
+            if (Age > 30) {
+                Console.WriteLine($"{Name} {Age}");
+            }
+        }
+    }
+}
---
+++ Modul 5/DefiningClasses/02.1/3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _3 {
+    internal class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new();
+            for (int i = 0; i < n; i++) {
+                string[] arguments = Console.ReadLine().Split(' ').Where(x => x != "").ToArray();
+                people.Add(new Person(arguments[0], Convert.ToInt32(arguments[1])));
+            }
+            people = people.OrderBy(x => x.Name).ToList();
+            foreach (Person person in people) {
+                person.PrintAbove30();
+            }
+        }
+    }
+
+}
