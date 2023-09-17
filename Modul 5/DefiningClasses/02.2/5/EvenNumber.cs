c95af9e6f64cb411174d653f54dfa7756cd45322
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-09-17 10:39:28 +0300

    added exercise 5And6 to DefiningClasses 02.2

---
+++ Modul 5/DefiningClasses/02.2/5/EvenNumber.cs
@@ -0,0 +1,18 @@
+﻿namespace _5 {
+    class EvenNumber {
+
+        private int num;
+
+        public int Num {
+            get => num;
+            set {
+                if (value % 2 == 0)
+                    num = value;
+            }
+        }
+
+        public EvenNumber(int num) {
+            Num = num;
+        }
+    }
+}
---
+++ Modul 5/DefiningClasses/02.2/5/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace _5 {
+    class Program {
+        static void Main(string[] args) {
+            List<EvenNumber> evenNumbers = Console.ReadLine().Split(' ').Where(x => x != "").Select(x => new EvenNumber(Convert.ToInt32(x))).Where(x => x.Num != 0).ToList();
+            Console.WriteLine(string.Join(", ", evenNumbers.Select(x => x.Num).ToList()));
+        }
+    }
+}
