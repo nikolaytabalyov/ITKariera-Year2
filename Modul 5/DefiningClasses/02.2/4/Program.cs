0bef4653c64d0dc53c982f4df792c7c9202d361b
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-09-17 10:13:44 +0300

    added exercise 4 to DefiningClasses 02.2

---
+++ Modul 5/DefiningClasses/02.2/4/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _4 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<RationalNumber> rationalNums = new();
+
+            int[] arguments = Console.ReadLine().Split(' ').Where(x => x != "").Select(int.Parse).ToArray();
+            for (int i = 0; i < arguments.Length; i += 2) {
+                rationalNums.Add(new RationalNumber(arguments[i], arguments[i + 1]));
+            }
+
+            RationalNumber.PrintList(rationalNums);
+        }
+    }
+}
---
+++ Modul 5/DefiningClasses/02.2/4/RationalNumber.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4 {
+    class RationalNumber : IComparable {
+
+        private int numerator;
+        private int denumerator;
+        public RationalNumber(int numerator, int denumerator) {
+            if (denumerator < 0) {
+                numerator *= -1;
+                denumerator *= -1;
+            }
+            int nod = BiggestDivider(numerator, denumerator);
+            Numerator = numerator/nod;
+            Denumerator = denumerator/nod;
+        }
+
+        public int Numerator { get; set; }
+        public int Denumerator {
+            get => denumerator;
+            set {
+                if (value == 0)
+                    throw new ArgumentException("Denumerator cannot be 0!");
+                else 
+                    denumerator = value;
+            }
+        }
+
+        public void Print() {
+            Console.Write($"{Numerator}/{Denumerator}");
+        }
+
+        public static void PrintList(List<RationalNumber> rationalNumbers) {
+            rationalNumbers = rationalNumbers.OrderBy(x => x).ToList();
+
+            foreach (var num in rationalNumbers) {
+                num.Print();
+                if (rationalNumbers.IndexOf(num) != rationalNumbers.Count - 1) {
+                    Console.Write("; ");
+                }
+            }
+        }
+
+        public int BiggestDivider(int numerator, int denumerator) {
+            int nod = 1;
+            int r = 0;
+            int a = Math.Abs(numerator);
+            int b = Math.Abs(denumerator);
+
+            while (b != 0) {
+                r = a % b;
+                a = b;
+                b = r;
+            }
+
+            nod = a;
+            return nod;
+        }
+
+        public int CompareTo(object? obj) {
+            RationalNumber x = (RationalNumber)obj;
+            if (Numerator * x.Denumerator < x.Numerator * Denumerator)
+                return -1;
+            if (Numerator * x.Denumerator == x.Numerator * Denumerator)
+                return 0;
+            else 
+                return 1;
+
+        }
+    }
+}
