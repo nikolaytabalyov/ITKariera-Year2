bf62d37c6ae593b008216b54a66a484f6cb208b4
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-09-18 09:57:40 +0300

    added exercise 3 to DefiningClasses 02.2

---
+++ Modul 5/Methods/03/03/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03 {
+    class BankAccount {
+
+        private int _id;
+        private decimal _balance;
+
+        public BankAccount(int id, decimal balance) {
+            ID = id;
+            Balance = balance;
+        }
+        public BankAccount(int id) {
+            ID = id;
+        }
+
+        public BankAccount() { }
+
+        public int ID { get => _id; set => _id = value; }
+        public decimal Balance { get => _balance; set => _balance = value; }
+
+        public void Deposit(decimal amount) => Balance += amount;
+        public void Withdraw(decimal amount) => Balance -= amount;
+        public override string ToString() => $"Account {ID}, balance {Balance}";
+    }
+}
---
+++ Modul 5/Methods/03/03/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace _03 {
+    internal class Program {
+        static void Main(string[] args) {
+            BankAccount acc = new();
+            BankAccount acc2 = new(123456, 50.0m);
+
+            Console.WriteLine(acc.ToString());
+            Console.WriteLine(acc2.ToString()); 
+        }
+    }
+}
