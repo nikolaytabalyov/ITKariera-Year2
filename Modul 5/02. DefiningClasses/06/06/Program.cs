ac69d2fe016c7a5bc544f051c7b400698c09c469
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2023-09-30 09:45:58 +0300

    rearanged the stucture of Defining classes

---
+++ Modul 5/02. DefiningClasses/06/06/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _06 {
+    class Program {
+        static void Main(string[] args) {
+
+            Person pesho = new("Peshov", "Peshov");
+            Console.WriteLine(Person.Counter);
+
+        }
+    }
+
+    public class Person {
+
+        private static int counter = 0;
+
+        public Person(string firstName, string lastName) {
+            FirstName = firstName;
+            LastName = lastName;
+            counter++;
+        }
+
+        public static int Counter => counter;
+
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+}
