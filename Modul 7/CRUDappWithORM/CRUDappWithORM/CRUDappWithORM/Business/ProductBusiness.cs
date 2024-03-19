997986ef80ef60a7c311f27fb4c098ec9657315f
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2024-03-19 19:57:09 +0200

    created the Data and Business parts of the CRUD app

--- Modul 7/CRUDappWithORM/CRUDappWithORM/CRUDappWithORM/Business/ProductBusiness.cs
+++ Modul 7/CRUDappWithORM/CRUDappWithORM/CRUDappWithORM/Business/ProductBusiness.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CRUDappWithORM.Data;
+using CRUDappWithORM.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +8,44 @@ using System.Threading.Tasks;
 
 namespace CRUDappWithORM.Business {
     public class ProductBusiness {
+        private ProductContext productContext;
+        public List<Product> GetAll() {
+            using (productContext = new ProductContext()) {
+                return productContext.Products.ToList();
+            }
+        }
+
+        public Product Get(int id) {
+            using (productContext = new ProductContext()) {
+                return productContext.Products.Find(id);
+            }
+        }
+
+        public void Add(Product product) {
+            using (productContext = new ProductContext()) {
+                productContext.Products.Add(product);
+                productContext.SaveChanges();
+            }
+        }
+
+        public void Update(Product product) {
+            using (productContext = new ProductContext()) {
+                var item = productContext.Products.Find(product.Id);
+                if (item != null) { 
+                    productContext.Entry(item).CurrentValues.SetValues(product);
+                    productContext.SaveChanges();
+                }
+            }
+        }
+        
+        public void Delete(Product product) {
+            using (productContext = new ProductContext()) {
+                var item = productContext.Products.Find(product.Id);
+                if (item != null) { 
+                    productContext.Products.Remove(product);
+                    productContext.SaveChanges();
+                }
+            }
+        }
     }
 }
--- Modul 7/CRUDappWithORM/CRUDappWithORM/CRUDappWithORM/Data/Models/Product.cs
+++ Modul 7/CRUDappWithORM/CRUDappWithORM/CRUDappWithORM/Data/Models/Product.cs
@@ -6,5 +6,9 @@ using System.Threading.Tasks;
 
 namespace CRUDappWithORM.Data.Models {
     public class Product {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
     }
 }
