9513a2fc4bc461eb1367ca39d401a7e3a5dccc49
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2024-03-19 21:50:37 +0200

    finished the Presentation part of the CRUD app

--- Modul 7/CRUDappWithORM/CRUDappWithORM/CRUDappWithORM/Program.cs
+++ Modul 7/CRUDappWithORM/CRUDappWithORM/CRUDappWithORM/Program.cs
@@ -1,7 +1,9 @@
-﻿namespace CRUDappWithORM {
+﻿using CRUDappWithORM.Presentation;
+
+namespace CRUDappWithORM {
     internal class Program {
         static void Main(string[] args) {
-
+            Display display = new Display();
         }
     }
 }
