6324d4b2e873fc4161179db77842cf3cab954630
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2024-03-20 20:40:31 +0200

    created a copy of the CRUDappWithORM to modify it with bigger database

---
+++ Modul 7/CRUDappWithORM++/CRUDappWithORM/CRUDappWithORM/Data/ProductContext.cs
@@ -0,0 +1,25 @@
+﻿using CRUDappWithORM.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace CRUDappWithORM.Data {
+    public class ProductContext : DbContext {
+        public ProductContext() 
+            : base() {
+
+        }
+        public DbSet<Product> Products { get; set; }    
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-9LO8QGB;Database=ProductDBv2;Integrated Security = true;Encrypt=false;");
+        }
+
+
+    }
+}
