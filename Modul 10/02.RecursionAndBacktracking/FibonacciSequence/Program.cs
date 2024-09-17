ee2dae9402505f339c4ac9a91991d6fbf30bd487
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2024-09-17 18:22:39 +0300

    Created sln for Recursion and added Fibonacci exrc

---
+++ Modul 10/02.RecursionAndBacktracking/FibonacciSequence/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FibonacciSequence {
+    internal class Program {
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(GetFibonacciNumber(n));
+        }
+
+        static int GetFibonacciNumber(int index) {
+            if (index <= 0) 
+                return 0;
+            if (index == 1 || index == 2) 
+                return 1;
+            else
+                return GetFibonacciNumber(index - 1) + GetFibonacciNumber(index - 2);
+        }
+    }
+}
