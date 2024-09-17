f97004da8aeafe3a9b579e9bfa9d325122c23b3e
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2024-09-17 18:49:24 +0300

    added exrc for GreatestCommonDivisor

---
+++ Modul 10/02.RecursionAndBacktracking/GreatestCommonDiviser/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace GreatestCommonDivisor {
+    internal class Program {
+        static void Main(string[] args) {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int gcdSubstraction = GetGreatestCommonDivisorWithSubstraction(a, b);
+            Console.WriteLine(gcdSubstraction);
+
+            int gcdDivision = GetGreatestCommonDivisorWithDivision(a, b);
+            Console.WriteLine(gcdDivision);
+        }
+
+        private static int GetGreatestCommonDivisorWithDivision(int a, int b) {
+            if (b == 0)
+                return a;
+            int r = b;
+            b = a % b;
+            a = r;
+            return GetGreatestCommonDivisorWithDivision(a, b);
+        }
+
+        private static int GetGreatestCommonDivisorWithSubstraction(int a, int b) {
+            if (a == b)
+                return a;
+            if (a > b)
+                return GetGreatestCommonDivisorWithSubstraction(a - b, b);
+            else
+                return GetGreatestCommonDivisorWithSubstraction(a, b - a);
+        }
+    }
+}
