9bb297e39060c1c1298891b9cef7eb5f313df24b
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2024-09-22 18:10:11 +0300

    did some work on the EightQueens exrc

---
+++ Modul 10/02.RecursionAndBacktracking/EightQueens/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace EightQueens {
+    internal class Program {
+        bool[,] board = new bool[8, 8];
+        bool[] attackedRows = new bool[8];
+        bool[] attackedColumns = new bool[8];
+        // top right -> top left -> bottom left    ld[i] = (n - 1) - col + row
+        bool[] attackedLeftDiagonals = new bool[8];
+        // top left -> top right -> bottom right   rd[i] = col + row
+        bool[] attackedRightDiagonals = new bool[8];
+
+        static void Main(string[] args) {
+
+        }
+        private bool CanPlaceQueen(int row, int col) {
+            if (attackedRows[row]
+                || attackedColumns[col]
+                || attackedLeftDiagonals[(attackedRows.Length - 1) - col + row]
+                || attackedRightDiagonals[col + row])
+                return false;
+            return true;
+        }
+
+        private void PlaceQueen(int row, int col) {
+            attackedRows[row] = true;
+            attackedColumns[col] = true;
+            attackedLeftDiagonals[(attackedRows.Length - 1) - col + row] = true;
+            attackedRightDiagonals[col + row] = true;
+        }
+    }
+}
