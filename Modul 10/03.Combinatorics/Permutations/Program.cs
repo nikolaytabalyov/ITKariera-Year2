ee73d284ec030dd83b0c8a0a06827a0b97dc8d07
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2024-09-26 18:33:24 +0300

    started work on Combinatorics with Permutations exrc

---
+++ Modul 10/03.Combinatorics/Permutations/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Permutations {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(Permutate(new int[]{ 0, 1, 2, 3, 4}));
+        }
+
+        static int Permutate(int[] numbers) {
+            // getting the number of combinations for one of the numbers except 0
+            bool containsZero = numbers.Contains(0);
+            int countOfNums = containsZero ? numbers.Length - 1 : numbers.Length;
+
+            int combinations = 1;
+            for (int i = 1; i <= countOfNums; i++) {
+                combinations *= i;
+            }
+
+            combinations = containsZero ? combinations * countOfNums : combinations;
+            return combinations;
+        }
+    }
+}
