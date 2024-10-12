a58d84ef671c73d474ed01f1d0cea52a102eaaae
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2024-10-12 18:42:30 +0300

    added LeastWalkedDistanceForFriends exrcs

---
+++ Modul 10/ExamExercises/01.LeastWalkedDistanceForFriends/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _01.LeastWalkedDistanceForFriends {
+    internal class Program {
+        static void Main(string[] args) {
+            List<int> streetNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            streetNumbers.Sort();
+            Console.WriteLine(streetNumbers[2] - streetNumbers[0]);
+        }
+    }
+}
