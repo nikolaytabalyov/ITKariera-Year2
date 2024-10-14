e840308a872f094ec022129d2efe56620f236f47
nikolaytabalyov <nikolay.tabalyov0@gmail.com>
2024-10-14 19:44:21 +0300

    added Exercises from the Module 10 Exam

---
+++ Modul 10/Exam13.10.24/01.MorseCode/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace _01.MorseCode {
+    internal class Program {
+        static List<string> codes = new List<string>();
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0 || n > 20)
+                throw new ArgumentException("Non valid n");
+            List<List<int>> combinations = new List<List<int>>();
+            List<int> indexes = codes.Select(x => codes.IndexOf(x)).ToList();
+            for (int i = 0; i < n; i++) {
+                string input = Console.ReadLine();
+                if (input.Length != n)
+                    throw new ArgumentException("Incorrect length of code");
+                
+                codes.Add(input);
+            }
+            GetCombinations(new int[n], 0, 1, n);
+        }
+
+        static void GetCombinations(int[] arr, int index, int start, int end) {
+            if (index >= arr.Length) {
+                for (int i = 0; i < arr.Length; i++)
+                    if (i < arr.Length - 1) Console.Write($"{codes[arr[i] - 1]}");//, arr[i]);
+                    else Console.Write(codes[arr[i] - 1]);//arr[i]);
+                Console.WriteLine();
+            } else {
+                for (int i = start; i <= end; i++) {
+                    arr[index] = i;
+                    GetCombinations(arr, index + 1, i, end);
+                }
+            }
+        }
+    }
+}
---
+++ Modul 10/Exam13.10.24/02.Reels/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class PeshoReels {
+    static void Main(string[] args) {
+        int N = int.Parse(Console.ReadLine()); // allocated time in seconds
+        int M = int.Parse(Console.ReadLine()); // number of reel length configurations
+
+        List<int> reelLengths = new List<int>();
+        for (int i = 0; i < M; i++) {
+            reelLengths.Add(int.Parse(Console.ReadLine()));
+        }
+
+        string command = Console.ReadLine();
+
+        if (command == "count") {
+            Console.WriteLine(MinimumReels(N, reelLengths));
+        } else if (command == "details") {
+            Dictionary<int, int> reelCounts = ReelCounts(N, reelLengths);
+            foreach (var kvp in reelCounts.OrderByDescending(kvp => kvp.Key)) {
+                if (kvp.Value != 0)  
+                    Console.WriteLine($"{kvp.Value} x {kvp.Key} seconds");
+            }
+        }
+    }
+
+    static int MinimumReels(int N, List<int> reelLengths) {
+        int totalReels = 0;
+        int remainingTime = N;
+
+        foreach (int reelLength in reelLengths) {
+            int reelsNeeded = remainingTime / reelLength;
+            totalReels += reelsNeeded;
+            remainingTime -= reelsNeeded * reelLength;
+        }
+
+        return totalReels;
+    }
+
+    static Dictionary<int, int> ReelCounts(int N, List<int> reelLengths) {
+        Dictionary<int, int> reelCounts = new Dictionary<int, int>();
+        int remainingTime = N;
+
+        foreach (int reelLength in reelLengths) {
+            int reelsNeeded = remainingTime / reelLength;
+            reelCounts[reelLength] = reelsNeeded;
+            remainingTime -= reelsNeeded * reelLength;
+        }
+
+        return reelCounts;
+    }
+}
